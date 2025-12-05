dc503a363f331137ffb4e3c90f6d41e20977f557
nelson-escalona <nelson.escalona@upr.edu>
2025-12-05 16:05:38 -0400

    Formatted (2.1)

--- src/balances.rs
+++ src/balances.rs
@@ -1,1 +1,11 @@
-pub struct Pallet {}
+use std::collections::BTreeMap;
+
+pub struct Pallet {
+	balances: BTreeMap<String, u128>,
+}
+
+impl Pallet {
+	pub fn new() -> Self {
+		Self { balances: BTreeMap::new() }
+	}
+}
