c0160d335096449ea73cdd633b8ebe6c0ee3ec20
nelson-escalona <nelson.escalona@upr.edu>
2025-12-05 21:41:42 -0400

    Created runtime struct and constructor (3.3)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 mod balances;
 mod system;
 
+pub struct Runtime {
+	system: system::Pallet,
+	balances: balances::Pallet,
+}
+
+impl Runtime {
+	fn new() -> Self {
+		Self { system: system::Pallet::new(), balances: balances::Pallet::new() }
+	}
+}
+
 fn main() {
 	println!("Hello, world!");
 }
