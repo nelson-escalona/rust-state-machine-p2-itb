fe921ea1149de2b1b38a5c263f14604bf381364b
nelson-escalona <nelson.escalona@upr.edu>
2025-12-06 00:30:06 -0400

    Created config trait for system to make it configurable (4.5)

--- src/system.rs
+++ src/system.rs
@@ -1,43 +1,49 @@
+use num::{One, Zero};
 use std::{collections::BTreeMap, ops::AddAssign};
 
-use num::{One, Zero};
-
-#[derive(Debug)]
-pub struct Pallet<AccountID, BlockNumber, Nonce> {
-	block_number: BlockNumber,
-	nonce: BTreeMap<AccountID, Nonce>,
+pub trait Config {
+	type AccountID: Ord + Clone;
+	type BlockNumber: Zero + One + AddAssign + Copy;
+	type Nonce: Zero + One + Copy;
 }
 
-impl<AccountID, BlockNumber, Nonce> Pallet<AccountID, BlockNumber, Nonce>
-where
-	AccountID: Ord + Clone,
-	BlockNumber: Zero + One + AddAssign + Copy,
-	Nonce: Zero + One + Copy,
-{
+#[derive(Debug)]
+pub struct Pallet<T: Config> {
+	block_number: T::BlockNumber,
+	nonce: BTreeMap<T::AccountID, T::Nonce>,
+}
+
+impl<T: Config> Pallet<T> {
 	pub fn new() -> Self {
-		Self { block_number: BlockNumber::zero(), nonce: BTreeMap::new() }
+		Self { block_number: T::BlockNumber::zero(), nonce: BTreeMap::new() }
 	}
 
-	pub fn block_number(&self) -> BlockNumber {
+	pub fn block_number(&self) -> T::BlockNumber {
 		self.block_number
 	}
 
 	pub fn inc_block_number(&mut self) {
-		self.block_number += BlockNumber::one();
+		self.block_number += T::BlockNumber::one();
 	}
 
-	pub fn inc_nonce(&mut self, who: &AccountID) {
-		let nonce: Nonce = *self.nonce.get(who).unwrap_or(&Nonce::zero());
-		let new_nonce = nonce + Nonce::one();
+	pub fn inc_nonce(&mut self, who: &T::AccountID) {
+		let nonce: T::Nonce = *self.nonce.get(who).unwrap_or(&T::Nonce::zero());
+		let new_nonce = nonce + T::Nonce::one();
 		self.nonce.insert(who.clone(), new_nonce);
 	}
 }
 
 #[cfg(test)]
 mod test {
+	struct TestConfig;
+	impl super::Config for TestConfig {
+		type AccountID = String;
+		type BlockNumber = u32;
+		type Nonce = u32;
+	}
 	#[test]
 	fn init_system() {
-		let mut system = super::Pallet::<String, u32, u32>::new();
+		let mut system = super::Pallet::<TestConfig>::new();
 		system.inc_block_number();
 		system.inc_nonce(&"alice".to_string());
 
