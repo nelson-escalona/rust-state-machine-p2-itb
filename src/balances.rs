6d6cd15b959eb439650b1b63a1caccdba7e12fb4
nelson-escalona <nelson.escalona@upr.edu>
2025-12-06 00:03:36 -0400

    Made pallet generic and moved type definitions to main.rs (4.3)

--- src/balances.rs
+++ src/balances.rs
@@ -1,14 +1,16 @@
+use num::traits::{CheckedAdd, CheckedSub, Zero};
 use std::collections::BTreeMap;
 
-type AccountID = String;
-type Balance = u128;
-
 #[derive(Debug)]
-pub struct Pallet {
+pub struct Pallet<AccountID, Balance> {
 	balances: BTreeMap<AccountID, Balance>,
 }
 
-impl Pallet {
+impl<AccountID, Balance> Pallet<AccountID, Balance>
+where
+	AccountID: Ord + Clone,
+	Balance: Zero + CheckedSub + CheckedAdd + Copy,
+{
 	pub fn new() -> Self {
 		Self { balances: BTreeMap::new() }
 	}
@@ -18,7 +20,7 @@ impl Pallet {
 	}
 
 	pub fn balance(&self, who: &AccountID) -> Balance {
-		*self.balances.get(who).unwrap_or(&0)
+		*self.balances.get(who).unwrap_or(&Balance::zero())
 	}
 
 	pub fn transfer(
@@ -30,8 +32,8 @@ impl Pallet {
 		let caller_balance = self.balance(&caller);
 		let to_balance = self.balance(&to);
 
-		let new_caller_balance = caller_balance.checked_sub(amount).ok_or("Not enough funds.")?;
-		let new_to_balance = to_balance.checked_add(amount).ok_or("Overflow")?;
+		let new_caller_balance = caller_balance.checked_sub(&amount).ok_or("Not enough funds.")?;
+		let new_to_balance = to_balance.checked_add(&amount).ok_or("Overflow")?;
 
 		self.balances.insert(caller, new_caller_balance);
 		self.balances.insert(to, new_to_balance);
@@ -44,7 +46,7 @@ impl Pallet {
 mod tests {
 	#[test]
 	fn init_balances() {
-		let mut balances = super::Pallet::new();
+		let mut balances = super::Pallet::<String, u128>::new();
 
 		assert_eq!(balances.balance(&"alice".to_string()), 0);
 		balances.set_balance(&"alice".to_string(), 100);
@@ -54,7 +56,7 @@ mod tests {
 
 	#[test]
 	fn transfer_balance() {
-		let mut balances = super::Pallet::new();
+		let mut balances = super::Pallet::<String, u128>::new();
 
 		assert_eq!(
 			balances.transfer("alice".to_string(), "bob".to_string(), 51),
