66a9c3260cfabebf84024abc6930a4d9422abae1
nelson-escalona <nelson.escalona@upr.edu>
2025-12-06 20:26:05 -0400

    created pallet level dispatch (6.3)

--- src/proof_of_existence.rs
+++ src/proof_of_existence.rs
@@ -39,6 +39,28 @@ impl<T: Config> Pallet<T> {
 	}
 }
 
+pub enum Call<T: Config> {
+	CreateClaim { claim: T::Content },
+	RevokeClaim { claim: T::Content },
+}
+
+impl<T: Config> crate::support::Dispatch for Pallet<T> {
+	type Caller = T::AccountId;
+	type Call = Call<T>;
+
+	fn dispatch(&mut self, caller: Self::Caller, call: Self::Call) -> DispatchResult {
+		match call {
+			Call::CreateClaim { claim } => {
+				self.create_claim(caller, claim)?;
+			},
+			Call::RevokeClaim { claim } => {
+				self.revoke_claim(caller, claim)?;
+			},
+		}
+		Ok(())
+	}
+}
+
 #[cfg(test)]
 mod test {
 	struct TestConfig;
